916c222dfc5678f1a9ca80e13ada08f5e3843d2f
pappijx <ayush.papani123@gmail.com>
2023-04-04 00:48:57 +0530

    add proper lint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -21,5 +21,11 @@ module.exports = {
   plugins: ['react', '@typescript-eslint', 'prettier'],
   rules: {
     'react/react-in-jsx-scope': 0,
+    'prettier/prettier': [
+      'error',
+      {
+        endOfLine: 'auto',
+      },
+    ],
   },
 };
